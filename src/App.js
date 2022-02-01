235d92eb3c3ec566471ee8e5b87336b107cc0e1b
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-01 23:59:45 +0300

    add useEffect to request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import HomePage from './components/HomePage';
@@ -6,18 +6,32 @@ import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
 
 import Header from './components/Header';
+import { connect, useDispatch } from 'react-redux';
+import { autoSignIn } from './actions';
 
 const App = (props) => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (!props.auth.isAuth) {
+			dispatch(autoSignIn());
+		}
+	}, [dispatch, props.auth.isAuth]);
+
 	return (
 		<Router>
-			<Header />
+			<Header {...props} />
 			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/' element={<HomePage {...props} />} />
+				<Route path='/login' element={<LoginPage {...props} />} />
+				<Route path='/signup' element={<SignUpPage {...props} />} />
 			</Routes>
 		</Router>
 	);
 };
 
-export default App;
+const mapStateToProps = (state) => {
+	return { auth: state.auth };
+};
+
+export default connect(mapStateToProps)(App);
