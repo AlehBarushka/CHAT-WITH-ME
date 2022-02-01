ef9108da62e0db21ffd65a0454731d9ed02128fd
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-01 23:57:46 +0300

    delete css file, added a ternary operator to display the login, logout, signin buttons

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,40 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './style.css';
-
 const Header = (props) => {
 	return (
 		<header className='flex items-center justify-between flex-wrap bg-blue-500 p-6'>
 			<Link to={'/'} className='font-semibold text-xl text-white tracking-tight'>
 				CHAT-WITH-ME
 			</Link>
-			<div>Hi, Aleh!</div>
+			<div className='font-semibold text-xl text-white tracking-tight'>{`Hello ${props.auth.userName}!`}</div>
 			<div>
-				<Link
-					to={'/login'}
-					className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
+				{props.auth.isAuth ? (
+					<button
+						className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
 						hover:border-transparent hover:text-blue-800 hover:bg-white mt-4 lg:mt-0'
-					onClick={props}
-				>
-					Login
-				</Link>
-				<Link
-					to={'/signup'}
-					className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
+						onClick={() => alert('logout')}
+					>
+						Logout
+					</button>
+				) : (
+					<>
+						<Link
+							to={'/login'}
+							className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
 						hover:border-transparent hover:text-blue-800 hover:bg-white mt-4 lg:mt-0'
-					onClick={props}
-				>
-					SignUp
-				</Link>
-				<Link
-					to={'#'}
-					className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
+						>
+							Login
+						</Link>
+						<Link
+							to={'/signup'}
+							className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
 						hover:border-transparent hover:text-blue-800 hover:bg-white mt-4 lg:mt-0'
-					onClick={props}
-				>
-					Logout
-				</Link>
+						>
+							SignUp
+						</Link>
+					</>
+				)}
 			</div>
 		</header>
 	);
