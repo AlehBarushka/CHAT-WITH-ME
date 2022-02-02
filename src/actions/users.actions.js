9de36df11cd9e66fae51c2700afaff2e0c43be73
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-02 23:17:19 +0300

    add unsubscribe promise  to realtime listener

--- src/actions/users.actions.js
+++ src/actions/users.actions.js
@@ -1,9 +1,9 @@
 import { userConstants } from './constatnts';
-import { collection, query, where, onSnapshot } from 'firebase/firestore';
+import { collection, query, onSnapshot, where } from 'firebase/firestore';
 import { db } from '../service/firebaseConfig';
 
 export const getRealTimeUsers = (uid) => {
-	return (dispatch) => {
+	return async (dispatch) => {
 		dispatch({ type: userConstants.GET_REALTIME_USERS_REQUEST });
 		const q = query(collection(db, 'users'), where('uid', '!=', uid));
 		const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -11,7 +11,6 @@ export const getRealTimeUsers = (uid) => {
 			querySnapshot.forEach((doc) => {
 				users.push(doc.data());
 			});
-			console.log(users);
 			dispatch({
 				type: userConstants.GET_REALTIME_USERS_SUCCESS,
 				payload: {
@@ -19,5 +18,6 @@ export const getRealTimeUsers = (uid) => {
 				},
 			});
 		});
+		return unsubscribe;
 	};
 };
