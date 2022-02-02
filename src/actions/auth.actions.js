8b154849c0e1ced74495d4407937e1301acfbfb7
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-02 15:17:48 +0300

    update doc(field - isOnline) after login and logout

--- src/actions/auth.actions.js
+++ src/actions/auth.actions.js
@@ -5,53 +5,55 @@ import {
 	signOut,
 	updateProfile,
 } from 'firebase/auth';
-import { collection, addDoc } from 'firebase/firestore';
+import { doc, setDoc, updateDoc } from 'firebase/firestore';
 import { auth, db } from '../service/firebaseConfig';
-import { authConstant } from './constatnts';
+import { authConstants } from './constatnts';
 
 export const signUp = (user) => {
-	return (dispatch) => {
+	return async (dispatch) => {
 		console.log('sigUP');
-		dispatch({ type: authConstant.USER_LOGIN_REQUEST });
+		dispatch({ type: authConstants.USER_LOGIN_REQUEST });
 		createUserWithEmailAndPassword(auth, user.email, user.password)
 			.then((response) => {
 				updateProfile(auth.currentUser, { displayName: user.userName });
-				addDoc(collection(db, 'users'), {
+				setDoc(doc(db, 'users', response.user.uid), {
 					userName: user.userName,
 					uid: response.user.uid,
 					createdAt: new Date(),
+					isOnline: true,
 				}).then(() => {
 					const loggedInUser = {
 						userName: user.userName,
 						uid: response.user.uid,
 						email: user.email,
 					};
-					// localStorage.setItem('user', JSON.stringify(loggedInUser));
-					dispatch({ type: authConstant.USER_LOGIN_SUCCESS, payload: { user: loggedInUser } });
+					dispatch({ type: authConstants.USER_LOGIN_SUCCESS, payload: { user: loggedInUser } });
 				});
 			})
 			.catch((error) => {
-				dispatch({ type: authConstant.USER_LOGIN_FAILURE, payload: { error: error.message } });
+				dispatch({ type: authConstants.USER_LOGIN_FAILURE, payload: { error: error.message } });
 			});
 	};
 };
 
 export const signIn = (user) => {
 	return (dispatch) => {
-		console.log('sigIn');
-		dispatch({ type: authConstant.USER_LOGIN_REQUEST });
+		dispatch({ type: authConstants.USER_LOGIN_REQUEST });
 		signInWithEmailAndPassword(auth, user.email, user.password)
 			.then((response) => {
+				const ref = doc(db, 'users', response.user.uid);
+				updateDoc(ref, {
+					isOnline: true,
+				});
 				const loggedInUser = {
 					userName: response.user.displayName,
 					uid: response.user.uid,
 					email: response.user.email,
 				};
-				// localStorage.setItem('user', JSON.stringify(loggedInUser));
-				dispatch({ type: authConstant.USER_LOGIN_SUCCESS, payload: { user: loggedInUser } });
+				dispatch({ type: authConstants.USER_LOGIN_SUCCESS, payload: { user: loggedInUser } });
 			})
 			.catch((error) => {
-				dispatch({ type: authConstant.USER_LOGIN_FAILURE, payload: { error: error.message } });
+				dispatch({ type: authConstants.USER_LOGIN_FAILURE, payload: { error: error.message } });
 			});
 	};
 };
@@ -59,7 +61,7 @@ export const signIn = (user) => {
 export const autoSignIn = () => {
 	return (dispatch) => {
 		console.log('autoSignIn');
-		dispatch({ type: authConstant.USER_LOGIN_REQUEST });
+		dispatch({ type: authConstants.USER_LOGIN_REQUEST });
 		onAuthStateChanged(auth, (user) => {
 			if (user) {
 				const loggedInUser = {
@@ -67,25 +69,27 @@ export const autoSignIn = () => {
 					uid: user.uid,
 					email: user.email,
 				};
-				dispatch({ type: authConstant.USER_LOGIN_SUCCESS, payload: { user: loggedInUser } });
+				dispatch({ type: authConstants.USER_LOGIN_SUCCESS, payload: { user: loggedInUser } });
 			} else {
-				dispatch({ type: authConstant.USER_LOGIN_FAILURE, payload: { error: 'Login again please' } });
+				dispatch({ type: authConstants.USER_LOGIN_FAILURE, payload: { error: 'Login again please' } });
 			}
 		});
 	};
 };
 
-export const logout = () => {
-	return (dispatch) => {
-		console.log('logOut');
-		dispatch({ type: authConstant.USER_LOGOUT_REQUEST });
-		signOut(auth)
-			.then(() => {
-				dispatch({ type: authConstant.USER_LOGOUT_SUCCESS });
-			})
-			.catch((error) => {
-				console.log(error);
-				dispatch({ type: authConstant.USER_LOGOUT_FAILURE, payload: { error: error.message } });
+export const logout = (uid) => {
+	return async (dispatch) => {
+		try {
+			await dispatch({ type: authConstants.USER_LOGOUT_REQUEST });
+			const ref = doc(db, 'users', uid);
+			await updateDoc(ref, {
+				isOnline: false,
 			});
+			await signOut(auth);
+			await dispatch({ type: authConstants.USER_LOGOUT_SUCCESS });
+		} catch (error) {
+			console.log(error);
+			dispatch({ type: authConstants.USER_LOGOUT_FAILURE, payload: { error: error.message } });
+		}
 	};
 };
