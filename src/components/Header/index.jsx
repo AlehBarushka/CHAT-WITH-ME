a96330526e2c982a38cd2fc047b01fd457cc7b47
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-02 23:18:26 +0300

    delete props and use hooks

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,20 +4,20 @@ import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../../actions/auth.actions';
 
-const Header = (props) => {
+const Header = () => {
 	const dispatch = useDispatch();
-	const uid = useSelector((state) => state.auth.uid);
+	const auth = useSelector((state) => state.auth);
 	const logoutUser = () => {
-		dispatch(logout(uid));
+		dispatch(logout(auth.uid));
 	};
 	return (
 		<header className='flex items-center justify-between flex-wrap bg-blue-500 p-6'>
 			<Link to={'/'} className='font-semibold text-xl text-white tracking-tight'>
 				CHAT-WITH-ME
 			</Link>
-			<div className='font-semibold text-xl text-white tracking-tight'>{`Hello ${props.auth.userName}!`}</div>
+			<div className='font-semibold text-xl text-white tracking-tight'>{`Hello ${auth.userName}!`}</div>
 			<div>
-				{props.auth.isAuth ? (
+				{auth.isAuth ? (
 					<button
 						className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
 						hover:border-transparent hover:text-blue-800 hover:bg-white mt-4 lg:mt-0'
