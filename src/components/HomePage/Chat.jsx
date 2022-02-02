fffbcef8fd9aae7b8a8cf8cc2e7b3a3ddaab55d7
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-02 15:21:09 +0300

    map users to users container

--- src/components/HomePage/Chat.jsx
+++ src/components/HomePage/Chat.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getRealTimeUsers } from '../../actions/users.actions';
 
 const Chat = () => {
+	const dispatch = useDispatch();
+	const auth = useSelector((state) => state.auth);
+	const users = useSelector((state) => state.users);
+
+	useEffect(() => {
+		dispatch(getRealTimeUsers(auth.uid));
+	}, [dispatch, auth.uid]);
+
 	return (
 		<>
 			<div className='mt-10 container mx-auto'>
@@ -34,20 +46,32 @@ const Chat = () => {
 						<ul className='overflow-auto h-[32rem]'>
 							<h2 className='my-2 mb-2 ml-2 text-lg text-gray-600'>Chats</h2>
 							<li>
-								<a className='flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none'>
-									<img
-										className='object-cover w-10 h-10 rounded-full'
-										src='https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg'
-										alt='username'
-									/>
-									<div className='w-full pb-2'>
-										<div className='flex justify-between'>
-											<span className='block ml-2 font-semibold text-gray-600'>Jhon Don</span>
-											<span className='block ml-2 text-sm text-gray-600'>онлайн</span>
-										</div>
-										<span className='block ml-2 text-sm text-gray-600'>bye</span>
-									</div>
-								</a>
+								{users.users.length > 0
+									? users.users.map((user) => {
+											return (
+												<Link
+													to='/'
+													key={user.uid}
+													className='flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none'
+												>
+													<img
+														className='object-cover w-10 h-10 rounded-full'
+														src='https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg'
+														alt='username'
+													/>
+													<div className='w-full pb-2'>
+														<div className='flex justify-between'>
+															<span className='block ml-2 font-semibold text-gray-600'>{user.userName}</span>
+															<span className='block ml-2 text-sm text-gray-600'>
+																{user.isOnline ? 'онлайн' : 'офлайн'}
+															</span>
+														</div>
+														<span className='block ml-2 text-sm text-gray-600'>bye</span>
+													</div>
+												</Link>
+											);
+									  })
+									: null}
 							</li>
 						</ul>
 					</div>
