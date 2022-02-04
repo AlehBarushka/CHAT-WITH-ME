d87cfc694032e647f42601318638096610f2bc09
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-05 01:39:27 +0300

    some update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -12,7 +12,13 @@ const Header = () => {
 	};
 	return (
 		<header className='flex items-center justify-between flex-wrap bg-blue-500 p-6'>
-			<Link to={'/'} className='font-semibold text-xl text-white tracking-tight'>
+			<Link
+				to={'/home'}
+				onClick={() => {
+					alert('переход на страницу');
+				}}
+				className='font-semibold text-xl text-white tracking-tight'
+			>
 				CHAT-WITH-ME
 			</Link>
 			<div className='font-semibold text-xl text-white tracking-tight'>{`Hello ${auth.userName}!`}</div>
@@ -20,7 +26,7 @@ const Header = () => {
 				{auth.isAuth ? (
 					<button
 						className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
-						hover:border-transparent hover:text-blue-800 hover:bg-white mt-4 lg:mt-0'
+						hover:border-transparent hover:text-blue-800 hover:bg-white'
 						onClick={logoutUser}
 					>
 						Logout
