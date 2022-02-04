b1309b7fa705abdbea580b6b441c16f26fba1cd9
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-05 01:37:18 +0300

    add new actions: add message to collections conversations, get realtime conversations under certain conditions

--- src/actions/users.actions.js
+++ src/actions/users.actions.js
@@ -1,5 +1,5 @@
 import { userConstants } from './constatnts';
-import { collection, query, onSnapshot, where } from 'firebase/firestore';
+import { collection, query, onSnapshot, where, addDoc, orderBy } from 'firebase/firestore';
 import { db } from '../service/firebaseConfig';
 
 export const getRealTimeUsers = (uid) => {
@@ -17,6 +17,41 @@ export const getRealTimeUsers = (uid) => {
 					users,
 				},
 			});
+		});
+		return unsubscribe;
+	};
+};
+
+export const updateMessage = (messageObj) => {
+	return async (dispatch) => {
+		let response = await addDoc(collection(db, 'conversations'), {
+			...messageObj,
+			isView: false,
+			createdAt: new Date(),
+		});
+		console.log(response);
+	};
+};
+
+export const getRealTimeConversations = (user) => {
+	return async (dispatch) => {
+		dispatch({ type: userConstants.GET_REALTIME_MESSAGES_REQUEST });
+		const q = query(
+			collection(db, 'conversations'),
+			where('senderUid', 'in', [user.senderUid, user.recipientUid]),
+			orderBy('createdAt', 'asc')
+		);
+		const unsubscribe = onSnapshot(q, (querySnapshot) => {
+			const conversations = [];
+			querySnapshot.forEach((doc) => {
+				if (
+					(doc.data().senderUid === user.senderUid && doc.data().recipientUid === user.recipientUid) ||
+					(doc.data().senderUid === user.recipientUid && doc.data().recipientUid === user.senderUid)
+				) {
+					conversations.push(doc.data());
+				}
+			});
+			dispatch({ type: userConstants.GET_REALTIME_MESSAGES_SUCCESS, payload: { conversations } });
 		});
 		return unsubscribe;
 	};
