3db6a3573497c97b8824399486bf6f8459764af7
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-05 01:41:36 +0300

    new avatars and delete offline markdown

--- src/components/HomePage/User.jsx
+++ src/components/HomePage/User.jsx
@@ -9,16 +9,16 @@ const User = (props) => {
 				className='flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none'
 			>
 				<img
-					className='object-cover w-10 h-10 rounded-full'
-					src='https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg'
+					className='object-cover w-8 h-8 rounded-full'
+					src='https://www.opensds.io/wp-content/uploads/sites/18/2019/03/user-unknown-1-300x300.png'
 					alt='username'
 				/>
 				<div className='w-full pb-2'>
 					<div className='flex justify-between'>
 						<span className='block ml-2 font-semibold text-gray-600'>{user.userName}</span>
-						<span className='block ml-2 text-sm text-gray-600'>{user.isOnline ? 'онлайн' : 'офлайн'}</span>
+						<span className={user.isOnline ? 'inline-block w-2 h-2 bg-green-400 rounded-full shadow' : ''}></span>
 					</div>
-					<span className='block ml-2 text-sm text-gray-600'>bye</span>
+					<span className='block ml-2 text-sm text-gray-600'>последнее сообщение</span>
 				</div>
 			</div>
 		</>
