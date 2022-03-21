5d749968e78a99fbb99054110caec7e4d20154d8
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-03-21 12:14:15 +0300

    deleted auth actions, reducer and root reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,1 @@
-export * from './auth.actions';
 export * from './conversations.action';
--- src/slices/index.js
+++
@@ -1,6 +0,0 @@
-import authReducer from '../reducers/auth.reducer';
-import conversationsReducer from '../reducers/conversations.reducer';
-
-const rootReducers = { auth: authReducer, conversations: conversationsReducer };
-
-export default rootReducers;
